891db4c0ba2b2229e146491c99116c5332618b4b
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-06-22 17:16:57 -0700

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+    def change 
+        create_table :costume_stores do |row|
+            row.string :name
+            row.string :location
+            row.integer :costume_inventory
+            row.boolean :whether_or_not_its_still_in_business
+            row.timestamp :opening_time
+            row.timestamp :closing_time
+        end
+    end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+    def change
+        create_table :haunted_houses do |row|
+            row.string :name
+            row.string :location
+            row.text :theme
+            row.integer :price
+            row.boolean :whether_theyre_family_friendly_or_not
+            row.timestamp :opening_date
+            row.timestamp :closing_date
+            row.string :long_description
+        end
+    end
+end
